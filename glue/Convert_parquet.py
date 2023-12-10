55625fcfa185f462f62ec4b4f5482e66e46f5488
Mateus Braga <mateusbraga@gmail.com>
2023-12-10 12:23:28 -0300

    Update Convert_parquet.py

--- glue/Convert_parquet.py
+++ glue/Convert_parquet.py
@@ -14,7 +14,12 @@ job.init(args["JOB_NAME"], args)
 
 # Script generated for node S3 ifsp-cd-staging/diabetes
 S3ifspcdstagingdiabetes_node1702128329450 = glueContext.create_dynamic_frame.from_options(
-    format_options={"quoteChar": '"', "withHeader": True, "separator": ","},
+    format_options={
+        "quoteChar": '"',
+        "withHeader": True,
+        "separator": ",",
+        "optimizePerformance": False,
+    },
     connection_type="s3",
     format="csv",
     connection_options={
@@ -32,7 +37,7 @@ S3ifspcdrawdiabetes_node1702128633924 = glueContext.write_dynamic_frame.from_opt
     connection_type="s3",
     format="glueparquet",
     connection_options={"path": "s3://ifsp-cd-raw/diabetes/", "partitionKeys": []},
-    format_options={"compression": "snappy"},
+    format_options={"compression": "gzip"},
     transformation_ctx="S3ifspcdrawdiabetes_node1702128633924",
 )
 
